e94e74c6be24232f798df7da83a544bd833fbc50
Jared Brown <brownjt01@sunyjcc.edu>
2018-10-08 20:41:07 -0400

    Added search (filter) capability to the quiz selection page

--- nodequiz/src/app/components/quiz-selection/quiz-selection.component.ts
+++ nodequiz/src/app/components/quiz-selection/quiz-selection.component.ts
@@ -9,8 +9,41 @@ import { Router } from '@angular/router';
 })
 export class QuizSelectionComponent implements OnInit {
   public availableQuizzes = this.demoService.getAvailableQuizzes();
+  public filterString = '';
+  private filteredQuizzes: Array<object> = [];
+  private searchString: string;
 
-  constructor(public demoService: DemoService) {}
+  constructor(public demoService: DemoService) {
+    this.BuildQuizzesArray();
+
+    this.searchString = '';
+  }
 
   ngOnInit() {}
+
+  filterQuizzes() {
+    if (this.searchString == '' || this.searchString == ' ') {
+      this.BuildQuizzesArray();
+    } else {
+      console.log('go');
+      let regexPrefix = '^';
+      let regexSuffix = '';
+      let regexString =
+        regexPrefix + this.searchString.toLowerCase() + regexSuffix;
+      let regexObject = RegExp(regexString);
+      this.filteredQuizzes = [];
+      this.availableQuizzes.forEach((v, i, a) => {
+        if (regexObject.test(v['title'].toLowerCase())) {
+          this.filteredQuizzes.push(v);
+        }
+      });
+    }
+  }
+
+  BuildQuizzesArray() {
+    this.filteredQuizzes = []; //We have to be sure array is empty
+    this.availableQuizzes.forEach((x) => {
+      this.filteredQuizzes.push(x);
+    });
+  }
 }
